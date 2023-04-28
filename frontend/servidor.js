31bf585f6e437050fd702217f5eb452e52e238c5
LeandroMelloo <leandromoreno1909@hotmail.com>
2023-04-27 22:21:14 -0300

    Finalizando integração com o SQS

--- frontend/servidor.js
+++ frontend/servidor.js
@@ -1,14 +1,40 @@
+const aws = require("aws-sdk");
 const express = require("express");
+
 const app = express();
 const porta = 3000;
 
 const pasta = process.env.PWD === undefined ? process.cwd() : process.env.PWD;
 
+// configuração da região na AWS
+aws.config.update({ region: "us-east-1" });
+
+// configuração do SQS
+const sqs = new aws.SQS();
+
 app.use(express.static(pasta));
 app.use(express.json());
 
 app.post("/solicitar_imagens", (request, response) => {
-  console.log(request.body);
+  const qtdImagens = parseInt(request.body.qtdImagens);
+
+  for (let i = 0; i < qtdImagens; i++) {
+    sqs.sendMessage(
+      {
+        MessageBody: "Imagem gerada SQS",
+        QueueUrl:
+          "https://sqs.us-east-1.amazonaws.com/789254738812/SolicitarImagens",
+      },
+      (erro, data) => {
+        if (erro) {
+          console.error(`Erro AWS SQS: ${erro}`);
+        } else {
+          console.log(`Sucesso: ${data.MessageId}`);
+        }
+      }
+    );
+  }
+
   response.json({ body: request.body, ok: true });
 });
 
