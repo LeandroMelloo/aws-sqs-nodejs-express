007d7a950f78b18f26e28a80f87a5c336991250f
LeandroMelloo <leandromoreno1909@hotmail.com>
2023-04-27 21:49:04 -0300

    Criando o endpoint de solicitar imagens

--- frontend/servidor.js
+++ frontend/servidor.js
@@ -5,6 +5,12 @@ const porta = 3000;
 const pasta = process.env.PWD === undefined ? process.cwd() : process.env.PWD;
 
 app.use(express.static(pasta));
+app.use(express.json());
+
+app.post("/solicitar_imagens", (request, response) => {
+  console.log(request.body);
+  response.json({ body: request.body, ok: true });
+});
 
 app.listen(porta, () => {
   console.log(`Servidor rodando na porta: ${porta}`);
